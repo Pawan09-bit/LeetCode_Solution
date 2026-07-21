d50edd0fa2a72f2eee5712e19e5fef612db2e79a
Pawan09-bit <pawanjoshi15102008@gmail.com>
2026-07-21 19:51:39 +0530

    Time: 6 ms (40.39%), Space: 46.7 MB (9.58%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> set =new TreeSet<>();
+        for(int n:nums){
+            set.add(n);
+            if(set.size() >3)  set.pollFirst();
+        }
+        return set.size() ==3 ? set.first() :set.last();
+    }
+}
