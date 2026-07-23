19bdedd91fe85efcb005e0e851df0421c6de0775
Pawan09-bit <pawanjoshi15102008@gmail.com>
2026-07-23 19:45:04 +0530

    Time: 1 ms (51.02%), Space: 125.4 MB (53.06%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+       
+        int n = nums.length;
+        if (n < 3) {
+            return n;
+        }
+        // Find the smallest power of 2 that is strictly greater than n
+        int ans = 1;
+        while (ans <= n) {
+            ans <<= 1; // Multiply by 2 using bitwise shift
+        }
+        
+        return ans;
+        
+    }
+}
